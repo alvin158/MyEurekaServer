1aa864714ba8efe5186204381dc48919455e8987
alvin158 <zhitat93@hotmail.com>
2021-08-30 18:01:01 +0800

    added AWS add-on

--- myeurekaserver/src/main/java/sg/edu/iss/myeurekaserver/MyeurekaserverApplication.java
+++ myeurekaserver/src/main/java/sg/edu/iss/myeurekaserver/MyeurekaserverApplication.java
@@ -1,8 +1,14 @@
 package sg.edu.iss.myeurekaserver;
 
+import com.netflix.appinfo.AmazonInfo;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -10,5 +16,14 @@ public class MyeurekaserverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyeurekaserverApplication.class, args);
+	}
+
+	@Bean
+	@Profile("!default")
+	public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
+		EurekaInstanceConfigBean b = new EurekaInstanceConfigBean(inetUtils);
+		AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
+		b.setDataCenterInfo(info);
+		return b;
 	}
 }
